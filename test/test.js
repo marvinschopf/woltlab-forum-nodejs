04af85302a471ec9009d03f349dce0e13c0e8339
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-07-14 21:23:09 +0200

    init client

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ describe('Client', () => {
   describe('#getIndex()', () => {
     it('should not be empty', (done) => {
       let success = false;
+      const client = new woltlab.Client('https://forum-raspberrypi.de/forum');
       const res = client.getIndex().then((content) => {
         if(content != null) success = true;
         assert.equal(success, true);
