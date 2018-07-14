c4cd07583d081621e646f8e1b3cbf8fecae27237
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-07-14 21:17:06 +0200

    fix test

--- test/test.js
+++ test/test.js
@@ -4,9 +4,10 @@ const woltlab = require("./../index");
 describe('Client', () => {
   describe('#getIndex()', () => {
     it('should not be empty', (done) => {
-      const client = new woltlab.Client("https://forum-raspberrypi.de/forum");
+      let success = false;
       const res = client.getIndex().then((content) => {
-        assert.notEqual(content, null);
+        if(content != null) success = true;
+        assert.equal(success, true);
         done();
       });
     });
